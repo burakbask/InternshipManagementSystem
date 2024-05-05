6870f1ae89f3c0c11bc5df1c11e94d4e758dca31
Kutay Akpınar <kutayakpnar@gmail.com>
2024-05-05 14:43:11 +0300

    Basic InternshipCoordinatorHome

--- src/components/InternshipCoordinatorHome.js
+++ src/components/InternshipCoordinatorHome.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/iyte_logo-tur.png'; // Adjust the path to where your logo is stored
+import '../styles/InternshipCoordinatorHome.css'
 function InternshipCoordinatorHome() {
   return (
-    <div>InternshipCoordinatorHome</div>
+    <div>
+      <nav className="navbar">
+        <img src={logo} className='logo' alt="Logo" />
+        <p className='ims-header'>INTERNSHIP MANAGEMENT SYSTEM</p>
+        <Link to="/" className="logout-button">Log Out</Link>
+      </nav>
+      <div>
+        InternshipCoordinatorHome Content Here
+      </div>
+    </div>
   )
 }
 
-export default InternshipCoordinatorHome
+export default InternshipCoordinatorHome;
