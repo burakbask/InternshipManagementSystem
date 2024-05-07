7aa6eb3e754c45aadf229ec41610cf0db78b9498
Kutay Akpınar <kutayakpnar@gmail.com>
2024-05-07 15:53:08 +0300

    InternshipCoordinatorHome Completed

--- src/components/InternshipCoordinatorHome.js
+++ src/components/InternshipCoordinatorHome.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../assets/iyte_logo-tur.png'; // Logo'nun saklandığı yere göre yolunu ayarlayın
+import { useNavigate } from 'react-router-dom';
+import { FaUpload, FaCheckCircle, FaBullhorn, FaFile } from 'react-icons/fa';
+import logo from '../assets/iyte_logo-tur.png';
 import '../styles/InternshipCoordinatorHome.css';
 
 function InternshipCoordinatorHome() {
+  const navigate = useNavigate();
+
+  const handleUploadClick = () => {
+    navigate("/InternshipCoordinatorDocumentsPage");
+  };
+
   return (
     <div>
       <nav className="navbar">
         <img src={logo} className='logo' alt="Logo" />
         <p className='ims-header'>INTERNSHIP MANAGEMENT SYSTEM</p>
-        <Link to="/" className="logout-button">Log Out</Link>
+        <div className="logout-button" onClick={() => navigate("/")}>Log Out</div>
       </nav>
-      <div className="content">
-        <Link to="/internshipCoordinatorDocumentsPage" className="big-div">
-          Upload Documents
-        </Link>
-        
+
+      <div className="outside-div">
+        <div className="big-div" onClick={handleUploadClick}>
+          <FaUpload className="icon" />
+          <div className="icon-text">Upload Document</div>
+        </div>
         <div className="big-div">
-          <p>Verify Internship</p>
-          {/* İkon gelecek */}
+          <FaCheckCircle className="icon" />
+          <div className="icon-text">Verify Internship</div>
+        </div>
+        <div className="big-div">
+          <FaBullhorn className="icon" />
+          <div className="icon-text">Announcements</div>
+        </div>
+        <div className="big-div">
+          <FaFile className="icon" />
+          <div className="icon-text">SSI Process</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default InternshipCoordinatorHome;
