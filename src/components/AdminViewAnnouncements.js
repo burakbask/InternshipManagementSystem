ee0557f0dfde97e48cd338facffb0e4606f0e182
Burak Can Baskın <burakcanbaskin2@gmail.com>
2024-05-25 17:24:56 +0300

    icon deleted

--- src/components/AdminViewAnnouncements.js
+++ src/components/AdminViewAnnouncements.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import '../styles/AdminViewAnnouncement.css';
 import logo from '../assets/iyte_logo-tur.png';
 import { Link } from 'react-router-dom';
-import internshipImage from '../assets/internshipimage.webp'; // Update the path to the new image file
+
 
 function AdminViewAnnouncements() {
   const [announcements, setAnnouncements] = useState([]);
@@ -37,7 +37,6 @@ function AdminViewAnnouncements() {
             announcements.map(announcement => (
               <div key={announcement.id} className="announcement-item">
                 <h2>Announcement Id:{announcement.id}</h2>
-                <img src={internshipImage} alt="Internship" className="announcement-image"/>
                 <p>You can view announcement.</p>
                 <p><small>{new Date(announcement.date).toLocaleString()}</small></p>
               </div>
